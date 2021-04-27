8dd765d6e50fdd84ca5eca282b0a00c9ee59b45b
pumaillekevin <pumaillekevin@gmail.com>
2021-04-27 00:08:21 -0500

    Mi primer commit

---
+++ src/test/java/edu/utp/chamcha/ChamchaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utp.chamcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
