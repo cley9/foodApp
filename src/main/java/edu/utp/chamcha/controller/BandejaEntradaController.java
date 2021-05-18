22363dc6d598c24752d5f7945fcbc99daf1ca222
cleygrup <u18204996 utp.edu.pe>
2021-05-18 03:11:30 -0500

    catalogo

--- src/main/java/edu/utp/chamcha/controller/BandejaEntradaController.java
+++ src/main/java/edu/utp/chamcha/controller/BandejaEntradaController.java
@@ -5,10 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BandejaEntradaController {
+public class BandejaEntradaController {/*
     public static String BANDEJA="cliente/Bandeja_Entrada";
     @GetMapping("/cliente/Bandeja_Entrada")
     public String index(Model model) {
         return BANDEJA;
-    } 
+    } */
 }
